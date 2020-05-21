de5b1ffd807553ec728097ceaf4e7ddaeb4bed41
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2020-05-21 23:59:50 +0530

    Done some ui part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import logo1 from './images/back.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="header">
+      <h1>HOME</h1>
+      </div>
+      <div className="body">
+        <div className="head">
+          <div className="logo">
+            <img src={logo1}></img>
+          
+          <div className="dropdown">
+          <button className="dropbtn"></button>
+              <div className="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+              </div>
+          </div>
+        </div>
+        <div className="down">
+          <div className="part1">
+            <h2>SR Bistro</h2>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
